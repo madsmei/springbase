212df17392b4dccac22049ca88a779407adcc861
madsmei <512641905@qq.com>
2020-04-25 21:28:13 +0800

    1.事件

---
+++ src/main/java/com/mads/initclass/EventListenerDeomo.java
@@ -0,0 +1,45 @@
+package com.mads.initclass;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+
+/**
+ * @Description: 这个是注解的方式  。另外一种 请看 {@link MyApplicationListener}
+ *
+ *      ps:需要 配置异步事件通道。如果不设置还是同步的。也就不是真正的发布订阅模式了。
+ *
+ * @Date 2020/4/25
+ * @Version V1.0
+ * @Author Mads
+ **/
+public class EventListenerDeomo {
+
+    class TestObject{
+        private Long id;
+        private String name;
+    }
+
+    @Autowired
+    private ApplicationContext context;
+
+
+    public void doString(TestObject c){
+        //do
+
+        //发布事件
+        context.publishEvent(c);
+    }
+
+
+    class EventListerDo{
+        /****
+         * 监听 事件，并处理
+         * @param c
+         */
+        @EventListener
+        public void doString(TestObject c){
+
+        }
+    }
+}
--- src/main/java/com/mads/initclass/MyApplicationListener.java
+++ src/main/java/com/mads/initclass/MyApplicationListener.java
@@ -5,11 +5,13 @@ import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
  * @Description: 当spring启动以后执行的逻辑，
+ *              @EventListener注解也能达到相同的效果， 请看 {@link EventListenerDeomo}
  * @Date 2020/4/15
  * @Version V1.0
  * @Author Mads
  **/
 public class MyApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
